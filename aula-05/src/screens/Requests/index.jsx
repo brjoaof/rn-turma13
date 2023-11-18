0097c5fe24b06e85bb046df1ad281a03cda805b7
João Felipe <brjoaof@gmail.com>
2023-11-17 21:03:02 -0300

    Api do Gatinho

--- aula-05/src/screens/Requests/index.jsx
+++ aula-05/src/screens/Requests/index.jsx
@@ -1,12 +1,35 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { View, Text, Button, Image, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+// import axios from "axios";
+import api from "../../services/api";
 
 const Requests = () => {
+  const [gatinho, setGatinho] = useState();
+  const [carregando, setCarregando] = useState(true);
+
   const getGatinho = async () => {
-    const response = await fetch("https://api.thecatapi.com/v1/images/search");
-    const dados = await response.json();
-    console.log(dados);
+    try {
+      setCarregando(true);
+      // const response = await fetch(
+      //   "https://api.thecatapi.com/v1/images/search"
+      // );
+      // const dados = await response.json();
+      // setGatinho(dados[0]);
+
+      const { data } = await api.get("/images/search");
+      setGatinho(data[0]);
+    } catch (e) {
+      console.log(e);
+    }
+
+    // fetch("https://api.thecatapi.com/v1/images/search")
+    //   .then((resp) => resp.json())
+    //   .then((data) => console.log(data));
   };
+
+  useEffect(() => {
+    getGatinho();
+  }, []);
 
   return (
     <View
@@ -19,7 +42,20 @@ const Requests = () => {
       <View>
         <Text>Requests</Text>
       </View>
-      <Button title="Fazer um GET" onPress={getGatinho} />
+
+      {/* {carregando && <ActivityIndicator size="large" color="blue" />} */}
+      {carregando && <ActivityIndicator size="large" color="blue" />}
+
+      <Image
+        // height={gatinho != undefined ? gatinho.height : null}
+        height={500}
+        width={400}
+        source={{
+          uri: gatinho?.url,
+        }}
+        onLoadEnd={() => setCarregando(false)}
+      />
+      <Button title="Fazer um GET" onPress={getGatinho} disabled={carregando} />
     </View>
   );
 };
